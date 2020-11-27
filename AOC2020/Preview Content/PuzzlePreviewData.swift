244a4591d052008139c998f376d71bb101f19c1e
Richard Pineo <Richard.E.Pineo@ge.com>
2020-11-27 15:43:48 -0800

    First commit

---
+++ AOC2020/Preview Content/PuzzlePreviewData.swift
@@ -0,0 +1,8 @@
+//
+//  PuzzlePreviewData.swift
+//  AOC2020
+//
+//  Created by Richard Pineo on 11/27/20.
+//
+
+import Foundation
---
+++ AOC2020/PuzzleProcessingView.swift
@@ -0,0 +1,20 @@
+//
+//  PuzzleProcessingView.swift
+//  AOC2020
+//
+//  Created by Richard Pineo on 11/27/20.
+//
+
+import SwiftUI
+
+struct PuzzleProcessingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PuzzleProcessingView_Previews: PreviewProvider {
+    static var previews: some View {
+        PuzzleProcessingView()
+    }
+}
