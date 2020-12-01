e27975eec137734843f33ef78b85bc984388ea74
Richard Pineo <Richard.E.Pineo@ge.com>
2020-11-30 21:38:34 -0800

    solve 1a

--- AOC2020Tests/TestSolve1.swift
+++ AOC2020Tests/TestSolve1.swift
@@ -2,8 +2,14 @@ import XCTest
 
 class TestSolve1: XCTestCase {
 
-    func testSolve1() throws {
+    func testExample() throws {
 		let solver = Solve1()
 		XCTAssertEqual(solver.example(), "514579")
     }
+
+	func testA() throws {
+		let solver = Solve1()
+		let solution = solver.a()
+		print(solution)
+	}
 }
