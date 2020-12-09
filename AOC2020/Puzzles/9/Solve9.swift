009c9a2524af97145141cec7721eab6110f12ccb
Richard Pineo <Richard.E.Pineo@ge.com>
2020-12-08 21:37:35 -0800

    solved 9

--- AOC2020/Puzzles/9/Solve9.swift
+++ AOC2020/Puzzles/9/Solve9.swift
@@ -18,30 +18,53 @@ class Solve9: PuzzleSolver {
 	}
 
 	func solveB() -> String {
-		""
+		solveB(inputFile, chunkSize: 25)
 	}
 
 	private func solve(_ filename: String, chunkSize: Int) -> String {
 		let values = FileHelper.load(filename)!.compactMap { Int($0) }
-		guard let invalid = findInvalid(values: values, chunkSize: chunkSize) else {
-			return "notfound"
+		if let invalid = findInvalid(values: values, chunkSize: chunkSize) {
+			return invalid.description
 		}
-		return values[invalid.endIndex].description
+		return "nope"
 	}
 	
 	private func solveB(_ filename: String, chunkSize: Int) -> String {
-	return ""
+		let values = FileHelper.load(filename)!.compactMap { Int($0) }
+		guard let invalid = findInvalid(values: values, chunkSize: chunkSize) else {
+			return "notfound"
+		}
+	
+		for start in 0..<values.count {
+			var check = 0
+			for count in start...values.count {
+				check = check + values[count]
+				if check == invalid {
+					// Found the invalid answer, crack the code.
+					let candidates = values[start...count]
+					let answer = candidates.min()! + candidates.max()!
+					return answer.description
+				}
+				else if check > invalid {
+					// kill the loop.
+					break
+				}
+			}
+		}
+		return "no answer found"
 	}
 	
-	private func findInvalid(values: [Int], chunkSize: Int) -> Range<Int>? {
+	private func findInvalid(values: [Int], chunkSize: Int) -> Int? {
 		for consider in chunkSize..<values.count {
 			let range = consider-chunkSize ..< consider
 			if !doesSumExist(values: values, value: values[consider], range: range) {
-				return range
+				return values[consider]
 			}
 		}
+		
 		return nil
 	}
+
 	
 	private func doesSumExist(values: [Int], value: Int, range: Range<Int>) -> Bool {
 		for first in range {
