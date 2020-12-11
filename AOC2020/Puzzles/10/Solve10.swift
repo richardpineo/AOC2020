eb7c9cc332270820ed65599dfe14b26fad808f6a
Richard Pineo <Richard.E.Pineo@ge.com>
2020-12-10 23:09:03 -0800

    missteps

--- AOC2020/Puzzles/10/Solve10.swift
+++ AOC2020/Puzzles/10/Solve10.swift
@@ -11,7 +11,7 @@ class Solve10: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		false
+		solveB(exampleFile) == "8" && solveB(exampleFile2) == "19208"
 	}
 
 	func solveA() -> String {
@@ -42,5 +42,66 @@ class Solve10: PuzzleSolver {
 		threeCount = threeCount + 1
 		let answer = oneCount * threeCount
 		return answer.description
+	}
+	
+	private func solveB(_ filename: String) -> String {
+		let lines = FileHelper.load(filename)!
+		let values = lines.compactMap { Int($0) }.sorted()
+		
+		let answer = branchCount(values: values)
+		let answer2 = recursiveAdapterCount(values: values, index: 0)
+		return answer.description
+		
+		var curIndex = 0
+		var combinatorics = 1
+		while curIndex < values.count - 1 {
+			// How many of 1...3 are in the next values
+			var nextIndex = curIndex + 1
+			while nextIndex < values.count && values[nextIndex] - values[curIndex] <= 3 {
+				nextIndex = nextIndex + 1
+			}
+			let possibleRoutes = nextIndex - curIndex - 1
+			
+			func cheapPow(_ count: Int) -> Int {
+				if count == 1 {
+					return 1
+				}
+				if count == 2 {
+					return 2
+				}
+				return 4
+			}
+			let newCombinatorics = cheapPow(possibleRoutes)
+			
+			print("collapsing \(values[curIndex])...\(values[nextIndex-1]). possible routes: \(possibleRoutes), new comb:  \(newCombinatorics). total: \(combinatorics)")
+
+			combinatorics = combinatorics * newCombinatorics
+			
+			curIndex = nextIndex - 1
+		}
+		return combinatorics.description
+	}
+
+	func branchCount(values: [Int]) -> Int {
+		var count = 0
+		for index in 0..<values.count {
+			let possibleNexts = (index + 1...index + 3).filter { next in next < values.count && values[next] - values[index] < 4 }
+			count = count + possibleNexts.count - 1
+		}
+		return count
+	}
+
+	func recursiveAdapterCount(values: [Int], index: Int) -> Int {
+		let currentValue = values[index]
+		var branchCount = 0
+		let possibleNexts = (index + 1...index + 3).filter { next in next < values.count && values[next] - currentValue < 4 }
+		branchCount += branchCount + possibleNexts.count - 1
+		
+		possibleNexts.forEach { next in
+			let branches = recursiveAdapterCount(values: values, index: next)
+		}
+		print("Count for value \(values[index]) is \(branchCount).")
+		
+		return branchCount
 	}
 }
